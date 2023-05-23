1c0b196a7b40bf4028c8258eceb80c6827fc0338
Fatih Karaca <fatih.karaca@gmx.net>
2023-05-23 08:50:44 +0200

    footer edit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,15 +5,19 @@ import { FaLinkedin, FaGithub } from 'react-icons/fa';
 function Footer() {
   return (
     <footer>
-      <p>Copyright &copy; 2023. All Rights Reserved.</p>
       <div>
-       <p>Created by F-Krc</p>
-      <Link to="https://github.com/F-Krc" className="icon" target="_blank">
-        <FaGithub />
-      </Link>
-      <Link to="https://www.linkedin.com/in/fatih-karaca-12ba58249" className="icon" target="_blank">
-        <FaLinkedin />
-      </Link> 
+        <p>Copyright &copy; 2023. All Rights Reserved.</p>
+      </div>
+      <div>
+        <p>Created by F-Krc</p>
+      </div>
+      <div>
+        <Link to="https://github.com/F-Krc" className="icon" target="_blank">
+          <FaGithub />
+        </Link>
+        <Link to="https://www.linkedin.com/in/fatih-karaca-12ba58249" className="icon" target="_blank">
+          <FaLinkedin />
+        </Link>
       </div>
     </footer>
   );
